25340d49efb552afc430176b3c80ca8db722e2a5
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-16 09:38:42 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+/*-----------------------------------------------------------------
+Challenge: 05-computeRemainder
+Difficulty: Basic
+Prompt:
+- Write a function named computeRemainder that accepts two numeric arguments and returns the remainder of the division of those two numbers.
+- The first argument should be the dividend and the second argument should be the divisor.
+- If a 0 is passed in as the second argument you should return JavaScript's special numeric value: Infinity.
+- For extra fun, complete this challenge without using the modulus (%) operator.
+Examples:
+computeRemainder(10,2) //=> 0
+computeRemainder(4,0) //=> Infinity
+computeRemainder(10.5, 3) //=> 1.5
+-----------------------------------------------------------------*/
+// Your solution for 05-computeRemainder:
+
+</div>
+`;
